4db71cf427fc86895110609fdbf08f3d0d8c877d
david-arias <devius123@gmail.com>
2025-04-29 10:53:44 -0500

    added dist build

--- frontEnd/src/comps/Debug/Debug.tsx
+++ frontEnd/src/comps/Debug/Debug.tsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from "react";
+
 import "./Debug.css";
 
 function Debug() {
     let winszw = window.innerWidth;
     let winszh = window.innerHeight;
+
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        // Optional: Update the date every second
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => clearInterval(intervalId); // Clean up interval on unmount
+    }, []);
+
     return (
         <>
             <div className="debugger">
-                {winszw}x{winszh}
+                size: {winszw}x{winszh}
+                <br />
+                {date.toDateString()}
             </div>
         </>
     );

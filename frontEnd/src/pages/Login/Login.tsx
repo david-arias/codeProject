c433ff08ceaeea1a3c57f0cfb198c022cebd5410
david-arias <devius123@gmail.com>
2025-04-30 10:00:18 -0500

    change manifest background

---
+++ frontEnd/src/pages/Login/Login.tsx
@@ -0,0 +1,19 @@
+import "./Login.css";
+// import { useState, useEffect } from "react";
+
+function Login() {
+    // const [Username, setUsername] = useState("");
+    // const [Pass, setPass] = useState("");
+
+    // useEffect(() => {}, []);
+
+    return (
+        <>
+            <div className="loginWrap">
+                <div className="topLogin"></div>
+            </div>
+        </>
+    );
+}
+
+export default Login;
